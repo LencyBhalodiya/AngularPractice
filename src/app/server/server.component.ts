66a95133956653aa69b64657e9ee9cf3068c7bca
lency <lbhalodiya@asite.com>
2023-02-18 20:46:54 +0530

    ngFor ngif ngstlye ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,20 +2,35 @@ import { Component } from "@angular/core"
 
 @Component({
     selector: 'app-server',
-    templateUrl: "./server.component.html"
+    templateUrl: "./server.component.html",
+    styles:[`
+    .flag{
+        border: 2px solid gray;
+    }
+    `]
 })
 
 export class Server{
     name:string  = "hostpot" //type script define variable
     topic = " is not there" 
     inputName:string = ""
+    flag:boolean = false;
+
+    servers = [];
+
     //function passing and accessing by string interpolation
 
    fun(){
     return this.topic;
    }
    onChange(){
+
+    this.servers.push(this.inputName);
     this.topic = 'Server is created'
+    this.flag = true;
+   }
+   getColor(){
+    return this.flag === false?'pink':'lightblue';
    }
    //this code is replaced by ngModel two way binding
 //    inputFn(e){
