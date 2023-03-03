1c535b7acd2cb135dcfca11318f37ad732b502c8
lency <lbhalodiya@asite.com>
2023-03-03 14:51:49 +0530

    services assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService } from './accounts.service';
-import { LoggingService } from './logging.service';
+import { Component } from '@angular/core';
+import { UserService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent implements OnInit{
-  accounts: {name:string, status:string}[] = []
+export class AppComponent {
 
-  constructor(private accountsService: AccountService){}
-
-  ngOnInit(): void {
-   this.accounts = this.accountsService.accounts; 
-  }
 }
