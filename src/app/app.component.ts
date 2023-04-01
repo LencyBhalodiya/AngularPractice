c7da30dd90c05fa299b951beed10e9b650a49f74
lency <lbhalodiya@asite.com>
2023-04-01 17:09:23 +0530

    initail auth setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(private fb: FormBuilder){}
-  genders = ['male', 'female'];
-  signupForm:FormGroup;
+export class AppComponent {
+  loadedFeature = 'recipe';
 
-  // can also use this method for reactive appraoch rather than creating new formgroup
-  // profileForm = this.fb.group({ 
-  //   firstName: [''],
-  //   lastName: [''],
-  //   address: this.fb.group({
-  //     street: [''],
-  //     city: [''],
-  //     state: [''],
-  //     zip: ['']
-  //   }),
-  // });
-
-  ngOnInit(){
-    this.signupForm = new FormGroup({ // to create group of specific form field
-      'userData': new FormGroup({
-        'username': new FormControl(null,Validators.required),
-        'email': new FormControl(null,[Validators.required ,Validators.email]),
-      }),
-      'gender': new FormControl('male'),
-      'hobbies': new FormArray([])
-    })
-  }
-  onAddHobby(){
-    const control = new FormControl(null,Validators.required);
-    (<FormArray>this.signupForm.get('hobbies')).push(control)
-  }
-  get controls() {
-    return (this.signupForm.get('hobbies') as FormArray).controls;
-  }
-  onSubmit(){
-    console.log(this.signupForm);
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
